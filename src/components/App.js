0a51c673f1d9a81961497c7d34bad17586606fa3
Sanan4li <Alisanan620@gmail.com>
2021-10-04 02:06:02 +0500

    added tailwindcss support

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,14 @@
 import React from 'react'
 
-import '../assets/css/App.css'
-
+import '../styles/main.css'
 function App() {
   return (
-    <div>
-      <h1>Hello, Electron!</h1>
+    <div className="mt-20">
+      <h1 className="font-bold text-4xl text-center text-gray-900">Hello, Electron!</h1>
 
-      <p>I hope you enjoy using basic-electron-react-boilerplate to start your dev off right!</p>
+      <p className="p-2 text-center text-lg">I hope you enjoy using basic-electron-react-boilerplate to start your dev off right!</p>
+
+      <p className="p-2 text-center">Added <span className="font-bold text-blue-600">TailwindCSS</span> Support by <span className="font-bold text-blue-600" >Sanan Ali</span></p>
     </div>
   )
 }
